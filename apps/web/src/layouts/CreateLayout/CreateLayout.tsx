56f15053c8d2eed9f35c3e0d6f6d00f06d91e745
Nadia Kiamilev <nadia@ourzora.com>
2023-02-28 14:32:37 +0100

    add page layout

--- apps/web/src/layouts/CreateLayout/CreateLayout.tsx
+++ apps/web/src/layouts/CreateLayout/CreateLayout.tsx
@@ -1,41 +1,11 @@
 import { Box } from '@zoralabs/zord'
 import React, { ReactElement, ReactNode } from 'react'
-import { useSigner } from 'wagmi'
 
-import { useDaoStore, useLayoutStore } from 'src/stores'
-import { getProvider } from 'src/utils/provider'
-
+import { PageLayout } from '../PageLayout'
 import { Nav } from './Nav'
 import { Uploading } from './Uploading'
 
 function CreateLayout({ children }: { children: ReactNode }) {
-  const { data: signer, status } = useSigner()
-  const { setSigner, setProvider, setSignerAddress } = useLayoutStore()
-  const { setIsMobile } = useLayoutStore()
-  const { addresses } = useDaoStore()
-
-  // store signer, signerAddress and provider in store
-  React.useEffect(() => {
-    if (status === 'success') {
-      setProvider(signer?.provider ?? getProvider())
-      setSigner(signer)
-      //@ts-ignore
-      setSignerAddress(signer?._address)
-    }
-  }, [status, signer, setProvider, addresses, setSigner, setSignerAddress])
-
-  // add mobile flag to layout store
-  React.useEffect(() => {
-    if (!!window) {
-      window.addEventListener('resize', handleResize)
-      setIsMobile(window.innerWidth <= 768)
-    }
-  }, [])
-
-  const handleResize = () => {
-    setIsMobile(window.innerWidth <= 768)
-  }
-
   return (
     <Box>
       <Nav />
@@ -46,5 +16,9 @@ function CreateLayout({ children }: { children: ReactNode }) {
 }
 
 export function getCreateLayout(page: ReactElement) {
-  return <CreateLayout>{page}</CreateLayout>
+  return (
+    <PageLayout>
+      <CreateLayout>{page}</CreateLayout>
+    </PageLayout>
+  )
 }
