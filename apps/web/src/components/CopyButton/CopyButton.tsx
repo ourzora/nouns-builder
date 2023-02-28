138883ee9d56ec5d2dd74ae6926704ac558e4b97
taayyohh <milesxbridges@gmail.com>
2023-02-28 09:25:40 -0500

    update imports

--- apps/web/src/components/CopyButton/CopyButton.tsx
+++ apps/web/src/components/CopyButton/CopyButton.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 import { Icon } from 'src/components/Icon'
 
-import { useDaoStore } from 'src/modules/dao''
+import { useDaoStore } from 'src/modules/dao'
 import { copyButtonVariants } from './CopyButton.css'
 
 interface CopyButtonProps {
