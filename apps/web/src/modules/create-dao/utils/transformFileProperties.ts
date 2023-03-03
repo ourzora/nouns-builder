e909b73159f178ca835474b806c4f1159d2fb674
taayyohh <milesxbridges@gmail.com>
2023-03-03 12:18:53 -0500

    test precommit hook

--- apps/web/src/modules/create-dao/utils/transformFileProperties.ts
+++ apps/web/src/modules/create-dao/utils/transformFileProperties.ts
@@ -125,7 +125,7 @@ export function transformFileProperties(
             ...uploadsToPropertyItems(remainingUploads, traitIndex, isNewProperty),
           ]
 
-          remainingUploads = []
+          remainingUploads = [];
         }
       }
     }
