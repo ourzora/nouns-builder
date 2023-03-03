70ad630b8f216f62974199399abbadce69b67afe
Nina Fletcher <nina@ourzora.com>
2023-03-03 13:17:24 +0100

    Handle 404s on dao pages

--- apps/web/src/layouts/DaoLayout.tsx
+++ apps/web/src/layouts/DaoLayout.tsx
@@ -1,3 +1,5 @@
+import { Flex } from '@zoralabs/zord'
+import { ethers } from 'ethers'
 import { useRouter } from 'next/router'
 import { ReactElement, ReactNode, useEffect } from 'react'
 import { useContractRead } from 'wagmi'
@@ -5,6 +7,7 @@ import { useContractRead } from 'wagmi'
 import { PUBLIC_MANAGER_ADDRESS } from 'src/constants/addresses'
 import { managerAbi } from 'src/data/contract/abis'
 import { useDaoStore } from 'src/modules/dao'
+import { notFoundWrap } from 'src/styles/404.css'
 import { AddressType } from 'src/typings'
 
 import { DefaultLayout } from './DefaultLayout'
@@ -38,6 +41,11 @@ function DaoLayout({ children }: { children: ReactNode }) {
   }, [setAddresses, data, token])
 
   if (!data) return null
+
+  const hasMissingAddresses = Object.values(data).includes(ethers.constants.AddressZero)
+  if (hasMissingAddresses) {
+    return <Flex className={notFoundWrap}>404 - Page Not Found</Flex>
+  }
 
   return <>{children}</>
 }
