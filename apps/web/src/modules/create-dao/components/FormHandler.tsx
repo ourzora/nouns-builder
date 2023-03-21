5c497daed7d59df60782864d93ec85357986ddc1
neokry <neokry.eth@gmail.com>
2023-03-21 13:51:49 -0700

    Add how to create a DAO link to first create page

--- apps/web/src/modules/create-dao/components/FormHandler.tsx
+++ apps/web/src/modules/create-dao/components/FormHandler.tsx
@@ -1,15 +1,47 @@
-import { Box, Heading, Text } from '@zoralabs/zord'
+import { Box, Flex, Heading, Text, atoms } from '@zoralabs/zord'
 import { motion } from 'framer-motion'
 import React from 'react'
 
+import { Icon } from 'src/components/Icon'
+
 import { CreateFormSection } from '../types'
 
-interface FormHandlerProps extends CreateFormSection {}
+interface FormHandlerProps extends CreateFormSection {
+  sectionIndex: number
+}
 
-export const FormHandler = ({ form, title, heading, subHeading }: FormHandlerProps) => {
+export const FormHandler = ({
+  form,
+  title,
+  heading,
+  subHeading,
+  sectionIndex,
+}: FormHandlerProps) => {
   return (
     <Box>
-      <Text variant={'eyebrow'}>Create A dao</Text>
+      <Flex align={'center'} justify={'space-between'}>
+        <Text variant={'eyebrow'}>Create A dao</Text>
+        {sectionIndex === 0 ? (
+          <a
+            href="https://docs.zora.co/docs/guides/builder-deployment"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Flex align={'center'} color={'text3'}>
+              <Text
+                fontWeight={'paragraph'}
+                fontSize={16}
+                className={atoms({ textDecoration: 'underline' })}
+              >
+                How to create a DAO?
+              </Text>
+              <Icon fill="text3" size="sm" ml="x1" id="external-16" />
+            </Flex>
+          </a>
+        ) : (
+          <></>
+        )}
+      </Flex>
       <Heading as={'h3'} mt={'x0'} mb={'x8'} fontSize={40}>
         {!heading ? title : heading}
       </Heading>
