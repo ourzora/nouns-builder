b773c6bfe173e6a2cbb30b732127317c29a47648
neokry <neokry.eth@gmail.com>
2023-03-31 14:46:49 -0700

    Add code and quote formatting

--- apps/web/src/modules/proposal/components/ProposalDescription/ProposalDescription.css.ts
+++ apps/web/src/modules/proposal/components/ProposalDescription/ProposalDescription.css.ts
@@ -7,6 +7,21 @@ globalStyle(`${proposalDescription} :is(h1, h2, h3, h4, h5, h6)`, {
   marginBottom: 6,
 })
 
-globalStyle(`${proposalDescription} p`, {
+globalStyle(`${proposalDescription} > p`, {
+  marginBottom: 20,
+})
+
+globalStyle(`${proposalDescription} code`, {
+  backgroundColor: 'rgba(0, 0, 0, 0.17)',
+  padding: '0.2em 0.4em',
+  borderRadius: '6px',
+  fontFamily: 'monospace!important',
+})
+
+globalStyle(`${proposalDescription} blockquote`, {
+  color: '#808080',
+  padding: '0 1em',
+  borderLeft: '0.25em solid #808080',
+  margin: 0,
   marginBottom: 20,
 })
