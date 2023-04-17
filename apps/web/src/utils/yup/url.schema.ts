93d54816044fbc36dcb6bf7fb00b576e6ca1cc95
neokry <neokry.eth@gmail.com>
2023-04-17 15:58:59 +0800

    Fixes DAO website validation

--- apps/web/src/utils/yup/url.schema.ts
+++ apps/web/src/utils/yup/url.schema.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup'
 
 const re =
-  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm
+  /^((ftp|http|https):\/\/)(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm
 
 export const urlValidationSchema = Yup.string()
   .transform((value: string) => value.replace(/\/$/, ''))
