16e0ccf4c4ccf4f800064decd1de737fbcdf52a6
neokry <neokry.eth@gmail.com>
2023-05-04 09:41:48 +0800

    Remove uneeded styles from explore skeleton

--- apps/web/src/modules/dao/components/Explore/ExploreSkeleton.tsx
+++ apps/web/src/modules/dao/components/Explore/ExploreSkeleton.tsx
@@ -4,19 +4,19 @@ import { exploreGrid, exploreSkeleton } from './Explore.css'
 
 export const ExploreSkeleton = () => {
   return (
-    <Grid className={exploreGrid} mb="x6">
+    <Grid className={exploreGrid} mb="x6" h="100%">
       {Array(6)
         .fill(0)
         .map((_, i) => (
-          <Box key={i} borderRadius="curved" height={'100%'} overflow="hidden">
-            <Box
-              backgroundColor="background2"
-              width={'100%'}
-              aspectRatio={1 / 1}
-              position="relative"
-              className={exploreSkeleton}
-            />
-          </Box>
+          <Box
+            key={i}
+            borderRadius="curved"
+            backgroundColor="background2"
+            width={'100%'}
+            aspectRatio={1 / 1}
+            position="relative"
+            className={exploreSkeleton}
+          />
         ))}
     </Grid>
   )
