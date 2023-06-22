b26dfe729a0d3a0d6c407e047b93b6c155498350
jordan <jordan.lesich@gmail.com>
2023-06-22 13:26:32 -0700

    add neverthrow to validate farcaster monadic errors, rewrite getDAOfeed

--- apps/web/src/constants/farcasterHub.ts
+++ apps/web/src/constants/farcasterHub.ts
@@ -1,2 +1,5 @@
-// export const FARCASTER_HUB_RPC = 'https://galaxy.ditti.xyz:2284'
-export const FARCASTER_HUB_RPC = 'nemes.farcaster.xyz:2283'
+export const FARCASTER_HUB_RPC = process.env.NEXT_FARCASTER_HUB
+
+// test data
+export const BUILDER_COLLECTION = '0xdf9b7d26c8fc806b1ae6273684556761ff02d422'
+export const PURPLE_COLLECTION = '0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60'
