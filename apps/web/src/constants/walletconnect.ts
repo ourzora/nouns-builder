77c6c1d784e8ea55fa6132438244221949340571
neokry <neokry.eth@gmail.com>
2023-06-27 09:16:45 +0800

    Change to single project id

--- apps/web/src/constants/walletconnect.ts
+++ apps/web/src/constants/walletconnect.ts
@@ -1,4 +1,1 @@
-export const PUBLIC_WALLLET_CONNECT_PROJECT_ID = {
-  1: '12fcc83f53d043bf4282e8233ef1aad7',
-  5: 'ce1e4f3420953e0b5cb8b1b5302cbb91',
-}[process.env.NEXT_PUBLIC_CHAIN_ID || 1] as string
+export const PUBLIC_WALLLET_CONNECT_PROJECT_ID = '12fcc83f53d043bf4282e8233ef1aad7'
