42e556603935ad73ff0786e4b94b3480e7e58aa5
jordan <jordan.lesich@gmail.com>
2023-07-03 19:31:08 -0700

    fix next env

--- apps/web/src/constants/farcasterHub.ts
+++ apps/web/src/constants/farcasterHub.ts
@@ -1,3 +1,2 @@
-//testnet will stop the error, it will not return feeds
-export const FARCASTER_HUB_RPC = process.env.NEXT_FARCASTER_HUB
-process.env.NEXT_FARCASTER_HUB || 'testnet1.farcaster.xyz:2283'
+export const BUILDER_COLLECTION = '0xdf9b7d26c8fc806b1ae6273684556761ff02d422'
+export const PURPLE_COLLECTION = '0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60'
--- apps/web/src/data/farcaster/client.ts
+++ apps/web/src/data/farcaster/client.ts
@@ -1,9 +1,12 @@
 import { getSSLHubRpcClient } from '@farcaster/hub-nodejs'
 
-import { FARCASTER_HUB_RPC } from 'src/constants/farcasterHub'
+export const BUILDER_COLLECTION = '0xdf9b7d26c8fc806b1ae6273684556761ff02d422'
+export const PURPLE_COLLECTION = '0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60'
 
 export const farcasterClient = () => {
-  const client = getSSLHubRpcClient(FARCASTER_HUB_RPC as string)
+  const client = getSSLHubRpcClient(
+    process.env.FARCASTER_HUB || 'testnet1.farcaster.xyz:2283'
+  )
 
   return client
 }
