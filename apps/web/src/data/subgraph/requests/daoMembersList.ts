ac1e1d3e5d58e4ccc351e4df152945929360b42c
jordan <jordan.lesich@gmail.com>
2023-07-05 23:27:49 -0700

    write daoMemberList Request

---
+++ apps/web/src/data/subgraph/requests/daoMembersList.ts
@@ -0,0 +1,41 @@
+import * as Sentry from '@sentry/nextjs'
+
+import { SDK } from 'src/data/subgraph/client'
+import { CHAIN_ID } from 'src/typings'
+
+import { DaoTokenOwner_OrderBy, OrderDirection } from '../sdk.generated'
+
+interface DaoMember {
+  id: string
+  daoTokenCount: number
+  timeJoined: string
+}
+
+export const membersListRequest = async (
+  chainId: CHAIN_ID,
+  collectionAddress: string
+): Promise<DaoMember[] | undefined> => {
+  try {
+    const data = await SDK.connect(chainId).daoMembersList({
+      where: {
+        dao: collectionAddress,
+      },
+      first: 10,
+      orderBy: DaoTokenOwner_OrderBy.DaoTokenCount,
+      orderDirection: OrderDirection.Desc,
+    })
+
+    if (!data.daotokenOwners) return undefined
+    return data.daotokenOwners.map((member) => ({
+      id: member.id,
+      daoTokenCount: member.daoTokenCount,
+      timeJoined: member.daoTokens
+        .map((daoToken) => daoToken.mintedAt)
+        .sort((a, b) => a - b)[0],
+    }))
+  } catch (error) {
+    console.error(error)
+    Sentry.captureException(error)
+    await Sentry.flush(2000)
+  }
+}
