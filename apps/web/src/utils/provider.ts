e057463459d069a46504a8fbca67850a95b3d992
neokry <neokry.eth@gmail.com>
2023-07-07 14:57:12 +0800

    Fix issue with get provider function

--- apps/web/src/utils/provider.ts
+++ apps/web/src/utils/provider.ts
@@ -4,12 +4,14 @@ import { ethers } from 'ethers'
 import { RPC_URL } from 'src/constants/rpc'
 import { CHAIN_ID } from 'src/typings'
 
-let provider: undefined | Provider
+let provider: Map<CHAIN_ID, Provider>
 
-export function getProvider(chain: CHAIN_ID): Provider {
-  if (!provider) {
+export function getProvider(chainId: CHAIN_ID): Provider {
+  if (!provider) provider = new Map()
+  if (!provider.has(chainId)) {
+    provider = new Map()
     // Use static provider to prevent re-querying for chain id since this won't change
-    provider = new ethers.providers.StaticJsonRpcProvider(RPC_URL[chain])
+    provider.set(chainId, new ethers.providers.StaticJsonRpcProvider(RPC_URL[chainId]))
   }
-  return provider
+  return provider.get(chainId)!
 }
