7f3a26c1e1cd7203574193c7d09220da74b2b20f
neokry <neokry.eth@gmail.com>
2023-07-14 15:06:02 +0800

    Add zora goerli testnet

--- apps/web/src/data/contract/chains.ts
+++ apps/web/src/data/contract/chains.ts
@@ -3,13 +3,13 @@ import { baseGoerli, goerli, mainnet, optimismGoerli } from 'wagmi/chains'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
-import { PUBLIC_IS_TESTNET } from 'src/constants/defaultChains'
+import { PUBLIC_IS_TESTNET, zoraGoerli } from 'src/constants/defaultChains'
 import { RPC_URL } from 'src/constants/rpc'
 import { CHAIN_ID } from 'src/typings'
 
 const MAINNET_CHAINS = [mainnet]
 
-const TESTNET_CHAINS = [goerli, optimismGoerli, baseGoerli]
+const TESTNET_CHAINS = [goerli, optimismGoerli, baseGoerli, zoraGoerli]
 
 const AVAILIBLE_CHAINS = PUBLIC_IS_TESTNET ? TESTNET_CHAINS : MAINNET_CHAINS
 
