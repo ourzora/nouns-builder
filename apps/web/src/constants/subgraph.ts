f7fea98e7f64ad8c95a240eb38a74554a4bee8a7
jordan <jordan.lesich@gmail.com>
2023-08-15 22:00:58 -0700

    recommit

--- apps/web/src/constants/subgraph.ts
+++ apps/web/src/constants/subgraph.ts
@@ -2,17 +2,17 @@ import { CHAIN_ID } from 'src/typings'
 
 export const PUBLIC_SUBGRAPH_URL = {
   [CHAIN_ID.ETHEREUM]:
-    'https://api.thegraph.com/subgraphs/name/neokry/nouns-builder-mainnet',
+    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-ethereum-mainnet/1.0.0/gn',
   [CHAIN_ID.OPTIMISM]:
     'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-optimism-mainnet/1.0.0/gn',
   [CHAIN_ID.GOERLI]:
-    'https://api.thegraph.com/subgraphs/name/neokry/nouns-builder-goerli',
+    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-goerli-testnet/1.0.0/gn',
   [CHAIN_ID.OPTIMISM_GOERLI]:
-    'https://api.thegraph.com/subgraphs/name/neokry/nouns-builder-optimism-goerli',
+    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-optimism-testnet/1.0.0/gn',
   [CHAIN_ID.BASE]:
     'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-base-mainnet/stable/gn',
   [CHAIN_ID.BASE_GOERLI]:
-    'https://api.studio.thegraph.com/query/49279/nouns-builder-base-goerli/version/latest',
+    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-base-testnet/1.0.0/gn',
   [CHAIN_ID.ZORA]:
     'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-zora-mainnet/stable/gn',
   [CHAIN_ID.ZORA_GOERLI]:
