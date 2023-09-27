c6215380abd551e54b72b207530eae753fd9624d
jordan <jordan.lesich@gmail.com>
2023-09-27 12:47:54 -0700

    expand functionality

--- apps/web/src/modules/dao/components/About/DaoDescription.tsx
+++ apps/web/src/modules/dao/components/About/DaoDescription.tsx
@@ -1,14 +1,27 @@
-import { Box } from '@zoralabs/zord'
-import React from 'react'
+import { Box, Button, Flex } from '@zoralabs/zord'
+import React, { useEffect, useRef } from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 import remarkGfm from 'remark-gfm'
 
-
-import { daoDescription } from './mdRender.css'
+import { daoDescription, fadingEffect } from './mdRender.css'
 
 export const DaoDescription = ({ description }: { description?: string }) => {
+  const [isOverHeight, setIsOverHeight] = React.useState(false)
+
+  const textRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    console.log('textRef.current', textRef.current?.clientHeight)
+    if (
+      textRef.current &&
+      textRef?.current?.scrollHeight > textRef?.current?.clientHeight
+    ) {
+      setIsOverHeight(true)
+    }
+  }, [])
+
   const correctedDescription = React.useMemo(() => {
     if (typeof description === 'string') {
       return description.replace(/\\n/g, '\n').replace(/\\r/g, '\r')
@@ -17,43 +30,38 @@ export const DaoDescription = ({ description }: { description?: string }) => {
   if (!correctedDescription) return null
 
   return (
-    <Box
-      mt={{ '@initial': 'x4', '@768': 'x6' }}
-      p="x6"
-      borderRadius={'phat'}
-      borderStyle={'solid'}
-      borderWidth={'normal'}
-      borderColor={'border'}
-    >
-      <ReactMarkdown
-        className={daoDescription}
-        rehypePlugins={[rehypeRaw, rehypeSanitize]}
-        remarkPlugins={[remarkGfm]}
-        // components={{
-        //   h1: ({ node, ...props }) => {
-        //     return (
-        //       <Heading size={'xl'} mb={'x6'} mt={'x6'}>
-        //         {props.children}
-        //       </Heading>
-        //     )
-        //   },
-        //   h2: ({ node, ...props }) => {
-        //     return (
-        //       <Heading size={'lg'} mb={'x4'} mt={'x4'}>
-        //         {props.children}
-        //       </Heading>
-        //     )
-        //   },
-        //   h3: ({ node, ...props }) => {
-        //     return <Heading size={'md'}>{props.children} </Heading>
-        //   },
-        //   p: ({ node, ...props }) => {
-        //     return <Text mb="x2">{props.children}</Text>
-        //   },
-        // }}
+    <Flex direction="column" align="flex-end">
+      <Box
+        mt={{ '@initial': 'x4', '@768': 'x6' }}
+        py="x2"
+        px={{ '@initial': 'x2', '@768': 'x4' }}
+        borderRadius={'phat'}
+        borderStyle={'solid'}
+        borderWidth={'normal'}
+        borderColor={'border'}
+        ref={textRef}
+        width="100%"
+        className={isOverHeight ? fadingEffect : ''}
+        style={{
+          maxHeight: isOverHeight ? '320px' : 'none',
+        }}
       >
-        {correctedDescription}
-      </ReactMarkdown>
-    </Box>
+        <ReactMarkdown
+          className={daoDescription}
+          rehypePlugins={[rehypeRaw, rehypeSanitize]}
+          remarkPlugins={[remarkGfm]}
+        >
+          {correctedDescription}
+        </ReactMarkdown>
+      </Box>
+      <Button
+        variant="ghost"
+        onClick={() => setIsOverHeight(!isOverHeight)}
+        size="sm"
+        px={'x0'}
+      >
+        {isOverHeight ? 'Read More' : 'Collapse'}
+      </Button>
+    </Flex>
   )
 }
