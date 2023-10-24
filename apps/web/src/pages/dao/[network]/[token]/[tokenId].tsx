a62877b55ccf164a6d7a12b6e421a04bea4c1266
Alex Grover <hello@alexgrover.me>
2023-10-23 23:40:27 -0700

    Handle undefined token image

--- apps/web/src/pages/dao/[network]/[token]/[tokenId].tsx
+++ apps/web/src/pages/dao/[network]/[token]/[tokenId].tsx
@@ -143,7 +143,7 @@ const TokenPage: NextPageWithLayout<TokenPageProps> = ({
           name,
           contractAddress: collection,
           chain,
-          image: token.image,
+          image: token.image || ogImageURL,
         }}
       />
 
