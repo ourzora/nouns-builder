c291df1ea661d6642d4bfe2f10c12eabc557bf44
neokry <neokry.eth@gmail.com>
2023-11-01 15:15:44 +0700

    add proof generation

--- apps/web/src/pages/api/allowList/[root].ts
+++ apps/web/src/pages/api/allowList/[root].ts
@@ -1,4 +1,4 @@
-import { getTree } from 'lanyard'
+import { getProof, getTree } from 'lanyard'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { decodeAbiParameters, parseAbiParameters } from 'viem'
 
@@ -7,6 +7,8 @@ import { AddressType, BytesType } from 'src/typings'
 export interface AllowListItem {
   claimant: AddressType
   tokenId: string
+  leaf: string
+  proof?: string[]
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -14,19 +16,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { root } = req.query
     if (!root) throw new Error('Invalid merkle root')
 
-    console.log('here')
-
     const leaves = await getTree(root as string).then((x) => x?.unhashedLeaves)
 
     if (!leaves) return res.status(200).send(null)
 
-    const parsedLeaves: AllowListItem[] = leaves.map((x) => {
-      const [claimant, tokenId] = decodeAbiParameters(
-        parseAbiParameters('address claimant, uint256 tokenId'),
-        x as BytesType
-      )
-      return { claimant, tokenId: tokenId.toString() }
-    })
+    const parsedLeaves: AllowListItem[] = await Promise.all(
+      leaves.map(async (x) => {
+        const [claimant, tokenId] = decodeAbiParameters(
+          parseAbiParameters('address claimant, uint256 tokenId'),
+          x as BytesType
+        )
+        const proof = await getProof({ merkleRoot: root as string, unhashedLeaf: x })
+
+        return { claimant, tokenId: tokenId.toString(), leaf: x, proof: proof?.proof }
+      })
+    )
 
     res.status(200).send(parsedLeaves)
   } catch (error) {
