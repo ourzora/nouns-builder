8c0d3722be5dfec4f6e522c6ba654cd61e917b60
bagelface <bagelface@protonmail.com>
2024-04-04 11:57:04 -0400

    Remove trailing forward slash for certain chain Etherscan URLs to fix broken links

--- apps/web/src/constants/etherscan.ts
+++ apps/web/src/constants/etherscan.ts
@@ -1,13 +1,14 @@
 import { CHAIN_ID } from 'src/typings'
 
+// URLs should not include a trailing forward slash
 export const ETHERSCAN_BASE_URL = {
   [CHAIN_ID.ETHEREUM]: 'https://etherscan.io',
   [CHAIN_ID.OPTIMISM]: 'https://optimistic.etherscan.io',
   [CHAIN_ID.SEPOLIA]: 'https://sepolia.etherscan.io',
-  [CHAIN_ID.OPTIMISM_SEPOLIA]: 'https://sepolia-optimism.etherscan.io/',
-  [CHAIN_ID.BASE]: 'https://basescan.org/',
-  [CHAIN_ID.BASE_SEPOLIA]: 'https://sepolia.basescan.org/',
-  [CHAIN_ID.ZORA]: 'https://explorer.zora.energy/',
-  [CHAIN_ID.ZORA_SEPOLIA]: 'https://sepolia.explorer.zora.energy/',
+  [CHAIN_ID.OPTIMISM_SEPOLIA]: 'https://sepolia-optimism.etherscan.io',
+  [CHAIN_ID.BASE]: 'https://basescan.org',
+  [CHAIN_ID.BASE_SEPOLIA]: 'https://sepolia.basescan.org',
+  [CHAIN_ID.ZORA]: 'https://explorer.zora.energy',
+  [CHAIN_ID.ZORA_SEPOLIA]: 'https://sepolia.explorer.zora.energy',
   [CHAIN_ID.FOUNDRY]: '',
 }
