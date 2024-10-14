258e5338619ed80901c6485556d19bd0efad5321
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-10-14 14:59:26 -0500

    fix extra spaces

--- apps/web/src/constants/rpc.ts
+++ apps/web/src/constants/rpc.ts
@@ -3,9 +3,9 @@ import { foundry } from 'wagmi/chains'
 import { CHAIN_ID } from 'src/typings'
 
 export const RPC_URL = {
-  [CHAIN_ID.ETHEREUM]: ` https://mainnet.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_RPC_KEY}`,
+  [CHAIN_ID.ETHEREUM]: `https://mainnet.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_RPC_KEY}`,
   [CHAIN_ID.OPTIMISM]: `https://optimism.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_RPC_KEY}`,
-  [CHAIN_ID.SEPOLIA]: ` https://sepolia.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_RPC_KEY}`,
+  [CHAIN_ID.SEPOLIA]: `https://sepolia.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_RPC_KEY}`,
   [CHAIN_ID.OPTIMISM_SEPOLIA]: `https://optimism-sepolia.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_RPC_KEY}`,
   [CHAIN_ID.BASE]: `https://base.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_RPC_KEY}`,
   [CHAIN_ID.BASE_SEPOLIA]: `https://base-sepolia.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_RPC_KEY}`,
