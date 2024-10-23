8ec0f2595675eb74ce8296b497e87081f9bbf474
Fazleiw <156914529+Fazleiw@users.noreply.github.com>
2024-10-23 11:07:57 +0700

    Refactoring for ESLint and Prettier Integration
    By adding the plugin:prettier/recommended and prettier/prettier rules,
    improved the configuration to ensure that Prettier formatting issues are handled by ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ['custom'],
+  extends: ['custom', 'plugin:prettier/recommended'],
   settings: {
     next: {
       rootDir: ['apps/*/'],
     },
+    },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
   },
 }
