d7038da014a8a7cc41a7c3b4ece9741164c86ac8
Dan OneTree <dan13ram@users.noreply.github.com>
2025-02-10 22:51:17 +0530

    PR 3: Adds support for gnosis safe app connector (#31)
    Builds upon #30

--- apps/web/src/data/contract/config.ts
+++ apps/web/src/data/contract/config.ts
@@ -1,18 +1,26 @@
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { connectorsForWallets, getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { safeWallet } from '@rainbow-me/rainbowkit/wallets'
 import { createConfig } from 'wagmi'
 
 import { PUBLIC_WALLLET_CONNECT_PROJECT_ID } from 'src/constants/walletconnect'
 
 import { chains, publicClient } from './chains'
 
-const { connectors } = getDefaultWallets({
+const { connectors: defaultConnectors } = getDefaultWallets({
   appName: 'Nouns builder',
   chains,
   projectId: PUBLIC_WALLLET_CONNECT_PROJECT_ID,
 })
 
+const safeConnector = connectorsForWallets([
+  {
+    groupName: 'Safe Wallet',
+    wallets: [safeWallet({ chains })],
+  },
+])
+
 export const config = createConfig({
   autoConnect: true,
-  connectors,
+  connectors: [...defaultConnectors(), ...safeConnector()],
   publicClient,
 })
